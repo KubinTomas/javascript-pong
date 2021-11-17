2e08ea66e9df5df272c22064e21b8a9dae45b2a7
Tomas Kubin <kubin.tom@email.cz>
2021-11-17 22:33:44 +0100

    add responsibility

--- scripts/pong.js
+++ scripts/pong.js
@@ -13,26 +13,26 @@ const workspace = document.getElementById('js-alg');
 const borderHeight = 20;
 
 const canvasManager = new CanvasManager();
-const canvasSize = new Size(workspace.offsetWidth, workspace.offsetHeight);
-const canvasSizeWithoutBorder = new Size(canvasSize.width, canvasSize.height - borderHeight * 2);
+let canvasSize = new Size(workspace.offsetWidth, workspace.offsetHeight);
+let canvasSizeWithoutBorder = new Size(canvasSize.width, canvasSize.height - borderHeight * 2);
 
-const canvas = canvasManager.getEmptyCanvas(new Rectangle(null, canvasSize));
+let canvas = canvasManager.getEmptyCanvas(new Rectangle(null, canvasSize));
 
 workspace.appendChild(canvas);
 
 const accesoriesColor = "#555555";
 
 const offsetX = 20;
-const borders = new Borders(canvasSize, borderHeight, accesoriesColor);
+let borders = new Borders(canvasSize, borderHeight, accesoriesColor);
 
-const bat1 = new Bat(new Rectangle(new Point(offsetX, borderHeight * 2), new Size(20, 150)), "#ecf0f1");
-const bat2 = new Bat(new Rectangle(new Point(canvas.width - offsetX * 2, borderHeight * 2), new Size(20, 150)), "#FFF");
+let bat1 = new Bat(new Rectangle(new Point(offsetX, borderHeight * 2), new Size(20, 150)), "#ecf0f1");
+let bat2 = new Bat(new Rectangle(new Point(canvas.width - offsetX * 2, borderHeight * 2), new Size(20, 150)), "#FFF");
 
 const ballWidth = 20;
-const ball = new Ball(new Rectangle(new Point(canvas.width / 2 - ballWidth / 2, canvas.height / 2 - ballWidth / 2), new Size(ballWidth, ballWidth)), "white");
+let ball = new Ball(new Rectangle(new Point(canvas.width / 2 - ballWidth / 2, canvas.height / 2 - ballWidth / 2), new Size(ballWidth, ballWidth)), "white");
 
-const middleLine = new MiddleLine(canvasSizeWithoutBorder, accesoriesColor, borderHeight);
-const score = new Score(canvasSize, 80, accesoriesColor, borderHeight);
+let middleLine = new MiddleLine(canvasSizeWithoutBorder, accesoriesColor, borderHeight);
+let score = new Score(canvasSize, 80, accesoriesColor, borderHeight);
 
 
 redraw();
@@ -66,19 +66,37 @@ document.addEventListener('keydown', (e) => {
 
 });
 
-// window.addEventListener('resize', function (event) {
-//     const width = window.innerWidth
-//         || document.documentElement.clientWidth
-//         || document.body.clientWidth;
+window.addEventListener('resize', function (event) {
+    const width = window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
 
-//     const height = window.innerHeight
-//         || document.documentElement.clientHeight
-//         || document.body.clientHeight;
+    const height = window.innerHeight
+        || document.documentElement.clientHeight
+        || document.body.clientHeight;
 
-//     workspace.width = width;
-//     workspace.height = height;
+    workspace.width = width;
+    workspace.height = height;
 
-//     redraw();
+    canvasSize = new Size(workspace.offsetWidth, workspace.offsetHeight);
+    canvasSizeWithoutBorder = new Size(canvasSize.width, canvasSize.height - borderHeight * 2);
+
+    canvas = canvasManager.getEmptyCanvas(new Rectangle(null, canvasSize));
+
+    workspace.innerHTML = '';
+    workspace.appendChild(canvas);
+
+    borders = new Borders(canvasSize, borderHeight, accesoriesColor);
+
+    bat1 = new Bat(new Rectangle(new Point(offsetX, borderHeight * 2), new Size(20, 150)), "#ecf0f1");
+    bat2 = new Bat(new Rectangle(new Point(canvas.width - offsetX * 2, borderHeight * 2), new Size(20, 150)), "#FFF");
+
+    ball = new Ball(new Rectangle(new Point(canvas.width / 2 - ballWidth / 2, canvas.height / 2 - ballWidth / 2), new Size(ballWidth, ballWidth)), "white");
+
+    middleLine = new MiddleLine(canvasSizeWithoutBorder, accesoriesColor, borderHeight);
+    score = new Score(canvasSize, 80, accesoriesColor, borderHeight);
+
+    redraw();
 
 
-// }, true);
+}, true);
